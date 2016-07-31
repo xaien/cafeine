548ad5e5edba24103337090b02fcb0823412c149
xvw <xavier.vdw@gmail.com>
2016-07-31 03:40:21 +0200

    Change SQL environement

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :cafeine, Cafeine.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "cafeine",
+  password: "cafeine",
   database: "cafeine_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :cafeine, Cafeine.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "cafeine_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
